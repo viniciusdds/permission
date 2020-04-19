82f2a3d10f389507252d17b73e1df80998313f2b
viniciusdds <viniciusdamasceno180@hotmail.com>
2020-04-18 21:18:32 -0300

    segunda commit

--- database/migrations/2020_04_14_114544_create_products_table.php
+++ database/migrations/2020_04_14_114544_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 255);
-            $table->string('email', 255)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 60);
-            $table->string('user_type')->default('user')->nullable();
-            $table->rememberToken();
+            $table->string('detalhe', 255);
+            $table->string('usuario', 255);
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 }
